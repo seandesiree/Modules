9c008752e26c0cdf74aa365a0690b39b6f1e8701
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2024-06-24 16:55:20 -0400

    update

---
+++ main.py
@@ -0,0 +1,4 @@
+import text_utils
+
+print(text_utils.capitalize_string("dancer"))
+print(text_utils.reverse_string("dancer"))
---
+++ mood_responses.py
@@ -0,0 +1,8 @@
+def mood_response(mood):
+    if mood == "happy".lower():
+        return "Another day of happiness. "
+    elif mood == "sad".lower():
+        return "Sadness is so important to feel. "
+    elif mood == "excited".lower():
+        return "Let it out. It can be infectitious. "
+    
